caa3e28dd99641510ca9a0a29a43ae9c067c29d9
frcarabali <fredy.junior@hotmail.com>
2022-05-23 02:53:17 -0500

    Se ingresan cambios primer pullrequest

--- src/exercise1.java
+++ src/exercise1.java
@@ -19,26 +19,26 @@ public class exercise1 {
 	*/
 	
 	public static void main (String[] args) {
-		String myString= "asfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342r";
-		char []	string_div = myString.toCharArray();
+		String miCadena= "asfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342rasfsdalgkjerio435j342234234nh234o32rnowei23io4h324oi32423o4w.,345.,34534534ewf342r";
+		char []	string_div = miCadena.toCharArray();
 		String n="";
-		String impair="";
-		int sum=0;
+		String impar="";
+		int suma=0;
 		
 			for (int i=0; i<string_div.length; i++) {
 				if(Character.isDigit(string_div[i])) {
 					n+=string_div[i];
 					if (string_div[i] %2 != 0) {
-						impair+= string_div[i];
+						impar+= string_div[i];
 					}
 				}
 			}	
 			System.out.println("1. Numeros enteros: "+n);
-			System.out.println("3. Numeros pares: "+impair);
+			System.out.println("3. Numeros pares: "+impar);
 			
-			for (int j=0; j<impair.length(); j++ ) {
-				sum += Integer.parseInt(String.valueOf(impair.charAt(j)));
+			for (int j=0; j<impar.length(); j++ ) {
+				suma += Integer.parseInt(String.valueOf(impar.charAt(j)));
 			}
-			System.out.println("4. Suma numeros: "+sum);
+			System.out.println("4. Suma numeros: "+suma);
 	}
 }
